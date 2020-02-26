a756e936524691fb899fbe585a2f73f11361814e
Andrei Bîscă <andreibsk+github@gmail.com>
2020-02-26 21:13:44 +0200

    Add compare snapshot on refresh

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { UiLayout } from "./UiLayout";
+import { Portfolio } from "./Portfolio";
 
 console.log('eToro Delta loaded.');
 
 const uiLayout = new UiLayout(document.querySelector(UiLayout.selector)!);
 
-uiLayout.portfolioAdded.attach(() => {
+uiLayout.portfolioAdded.attach((p: Portfolio) => {
 	console.debug("Portfolio added.");
+	const snapshot = p.createSnapshot();
+	p.compareSnapshot = snapshot;
 })
 
 uiLayout.portfolioRemoved.attach(() => {
