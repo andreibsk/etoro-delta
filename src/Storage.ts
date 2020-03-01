7b8ea733301f2acea0820b339859b5a0f5eb6e64
Andrei Bîscă <andreibsk+github@gmail.com>
2020-03-01 11:58:52 +0200

    Implement storage actions

---
+++ src/Storage.ts
@@ -0,0 +1,44 @@
+import { browser } from "webextension-polyfill-ts";
+
+const key = {
+    snapshotDates: "snapshotDates",
+    
+	snapshotPrefix: "snapshot.",
+	snapshot: (date: Date) => "snapshot." + date.getTime()
+};
+
+class SyncStorage {
+    public async getSnapshotDates(): Promise<Date[]> {
+        return this.getSnapshotDateTimes()
+            .then(times => times.map(n => new Date(n)));
+    }
+
+    public async getSnapshot<TSnapshot>(date: Date): Promise<TSnapshot | undefined> {
+        const snapshotKey = key.snapshot(date);
+        const storageItems = await browser.storage.sync.get(snapshotKey);
+
+        return storageItems[snapshotKey];
+    }
+
+    public async addSnapshot<TSnapshot>(snapshot: TSnapshot): Promise<void> {
+        const date = new Date();
+
+        const snapshotDateTimes = await this.getSnapshotDateTimes();
+        snapshotDateTimes.push(date.getTime());
+
+        const snapshotKey = key.snapshot(date);
+
+        await browser.storage.sync.set({
+            [key.snapshotDates]: snapshotDateTimes,
+            [snapshotKey]: snapshot
+        });
+    }
+
+    private async getSnapshotDateTimes(): Promise<number[]> {
+        const storageItems = await browser.storage.sync.get(key.snapshotDates);
+
+        return (storageItems[key.snapshotDates] ?? []) as number[];
+    }
+}
+
+export const storage = new SyncStorage();
