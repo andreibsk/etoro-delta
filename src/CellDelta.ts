f2265e5e5f6a64eb973963cb4475cd493a780920
Andrei Bîscă <andreibsk+github@gmail.com>
2020-03-02 19:37:34 +0200

    Add delta arrow animation

--- src/CellDelta.ts
+++ src/CellDelta.ts
@@ -1,14 +1,19 @@
+import styles from "./main.scss";
+
 export class CellDelta {
     public readonly element: Element;
+
+    private _value: number = 0.00;
+    private readonly arrowElement: HTMLElement;
 
     constructor(value?: number) {
         value = value ?? 0;
 
-        this.element = document.createElement("div");
+        this.arrowElement = document.createElement("span");
 
-        const arrow = document.createElement("span");
-        arrow.className = "arrow-sign";
-        this.element.appendChild(arrow);
+        this.element = document.createElement("div");
+        this.element.className = styles.delta;
+        this.element.appendChild(this.arrowElement);
         this.element.appendChild(document.createTextNode("0.00"));
 
         this.value = value;
@@ -16,6 +21,18 @@ export class CellDelta {
 
     public set value(value: number) {
         this.element.childNodes[1].textContent = value == 0 ? "-" : value.toFixed(2);
-        this.element.className = value > 0 ? "positive" : (value < 0 ? "negative" : "dark");
+
+        const valueClass = value > 0 ? "positive" : (value < 0 ? "negative" : "dark");
+        if (!this.element.classList.contains(valueClass)) {
+            this.element.classList.remove("positive", "negative", "dark");
+            this.element.classList.add(valueClass);
+        }
+
+        const valueDelta = value - this._value;
+        this.arrowElement.className = "";
+        void this.arrowElement.offsetWidth;
+        this.arrowElement.className = styles[valueDelta > 0 ? "arrowSignPositive" : "arrowSignNegative"];
+
+        this._value = value;
     }
 }
